9caa7e954bcd5d49549ae4f2c8eb57d9ceb07426
kutec <ku@kutec.co.in>
2019-09-16 14:03:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 01-modular-js-jquery-need
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/01-modular-js-jquery-need)
---
+++ index.js
@@ -0,0 +1,28 @@
+// Import stylesheets
+import './style.css';
+
+// jquery
+import $ from "jquery";
+import Mustache from 'mustache';
+
+// Write Javascript code!
+
+var people = [];
+var template = $('#people-template').html();
+
+$('#peopleModule').find('button').on('click', function(){
+  people.push($('#peopleModule').find('input').val());
+  $('#peopleModule').find('input').val('');
+  var data = {
+    people = people
+  };
+  $('#peopleModule').find('ol').html(Mustache.render(template, data));
+});
+
+$('#peopleModule').find('ol').delegate('i.del', 'click', function(e){
+  var $remove = $(e.target).closest('li');
+  var i = $('#peopleModule').find('ol').find('li').index($remove);
+
+  $remove.remove();
+  people.splice(i, 1);
+});
