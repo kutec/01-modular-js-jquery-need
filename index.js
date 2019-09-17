0c7eb2485520e03e8a16296c9933c556d1c4648b
kutec <ku@kutec.co.in>
2019-09-17 13:02:23 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -5,23 +5,5 @@ import './style.css';
 import $ from "jquery";
 import Mustache from 'mustache';
 
-// Write Javascript code!
-var people = [];
-var template = $('#people-template').html();
-
-$('#peopleModule').find('button').on('click', function(){
-  people.push($('#peopleModule').find('input').val());
-  $('#peopleModule').find('input').val('');
-  var data = {
-    people = people
-  };
-  $('#peopleModule').find('ol').html(Mustache.render(template, data));
-});
-
-$('#peopleModule').find('ol').delegate('i.del', 'click', function(e){
-  var $remove = $(e.target).closest('li');
-  var i = $('#peopleModule').find('ol').find('li').index($remove);
-
-  $remove.remove();
-  people.splice(i, 1);
-});
+// modules
+import './modules/people.js';
